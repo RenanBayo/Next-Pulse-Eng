1820f4272a441c5332e556140012a2f8844eaf22
RenanBayo <148905816+RenanBayo@users.noreply.github.com>
2025-04-12 21:46:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once (__DIR__ . '/hpdv.com.br/app/layouts/session.php');
+require_once (__DIR__ . '/hpdv.com.br/app/web/login.php');
 
 // Redirecionar para o dashboard
 header("Location: dashboard.php");
